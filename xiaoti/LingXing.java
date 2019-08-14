c71f979a7a76c0f312aed674ef696dc49c1b650a
liangkai-web <53181965+liangkai-web@users.noreply.github.com>
2019-08-14 19:00:34 +0800

    Update LingXing.java

--- xiaoti/LingXing.java
+++ xiaoti/LingXing.java
@@ -1,9 +1,19 @@
+打印出如下图案（菱形） 
+      * 
+    *** 
+  ***** 
+******* 
+  ***** 
+    *** 
+      * 
+1.程序分析：先把图形分成两部分来看待，前四行一个规律，后三行一个规律，利用双重 for循环，第一层控制行，第二层控制列。
+
 package zuoye;
 
 public class LingXing {
 	public static void main(String[] args) {
 		int i,j;
-		//��ӡ���Ĳ����������
+		//打印上四层等腰三角形
 		for(i=0;i<=3;i++)
 		{
 		for(j=0;j<=2-i;j++)
@@ -12,7 +22,7 @@ public class LingXing {
 					System.out.print("*");
 				System.out.print("\n");
 				}
-				//��ӡ���������������
+				//打印下三层等腰三角形
 				for(i=0;i<=2;i++)
 				{
 				for(j=0;j<=i;j++)
